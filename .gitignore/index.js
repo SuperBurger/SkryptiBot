800be6eb90de42876371e15af2c0397ebad32c61
SuperBurger <41854541+SuperBurger@users.noreply.github.com>
2018-07-29 10:26:05 +0200

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -2,9 +2,19 @@ const Discord = require("discord.js");
 
 var bot = new Discord.Client();
 
+const PREFIX = "§";
+
 bot.on("ready", function(){
     bot.user.setGame("§help pour de l'aide !");
     console.log("Le Bot est bien connecté !");
+})
+
+bon.on('message', message =>{
+    if(message.content[0] === PREFIX) {
+       if(message.content === '§Bonjour') {
+           message.reply ('Salut ! Comment ça va ?');
+       } 
+    }
 })
 
 bot.login("NDczMDI4ODg2NjcwODY4NDgx.Dj8AEQ.dTEdp2fvEarMUlYKzR61TwPeTY0");
